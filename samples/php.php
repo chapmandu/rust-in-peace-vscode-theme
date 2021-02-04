3a93cb07465135827edee83758c3013ebd30ff01
Adam Chapman <adam.p.chapman@gmail.com>
2021-02-04 13:13:00 +1100

    gutter colours

--- samples/php.php
+++ samples/php.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 
+
+
+
 class UserController extends Controller
 {
     /**
@@ -12,7 +15,8 @@ class UserController extends Controller
      *
      * @param  int  $id
      * @return \Illuminate\View\View
-     */
+
+
     public function show($id)
     {
         return view('user.profile', [
