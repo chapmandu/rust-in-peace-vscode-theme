0e52af36b6233a84445395bd63af78fc8bf95eea
Adam Chapman <adam.p.chapman@gmail.com>
2023-05-13 20:19:54 +1000

    update .py sample

--- samples/py.py
+++ samples/py.py
@@ -1,20 +1,27 @@
 # Calculator
 
+
 # This function adds two numbers
 def add(x, y):
     return x + y
+
 
 # This function subtracts two numbers
 def subtract(x, y):
     return x - y
 
+
 # This function multiplies two numbers
 def multiply(x, y):
     return x * y
 
+
 # This function divides two numbers
 def divide(x: float, y: float) -> float:
     return x / y
+
+
+r: set[str] = search_for_vowels("Raining Blood")
 
 print("Select operation.")
 print("1.Add")
